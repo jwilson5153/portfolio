11741972c09dc8c446aa1fca323493c70acc7b4c
jwilson5153 <jwilson5153@yahoo.com>
2021-04-22 09:00:35 -0500

    20210422Update1

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/* Obtain Dynamic Table of Contents */
+const link_listURL = 'json/link_list.json'
+fetch(link_listURL)
+    .then(function (response) {
+        return response.json();
+    
+    })
+    .then(function (jsonObject) {
+        const link_list = jsonObject['link_list'];
+        link_list.forEach(item => {
+            const menu_link  = document.createElement("li"); 
+            const link_list = document.querySelector("#link_list");
+            menu_link.innerHTML = `<a href="${item.url}">  ${item.label}</a>`;
+            link_list.append(menu_link);
+
+        })
+    })
