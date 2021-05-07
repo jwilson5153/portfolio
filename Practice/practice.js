9ddc888e1001fbf994aa6fde17ed162bd5fffe51
jwilson5153 <jwilson5153@yahoo.com>
2021-05-07 10:30:12 -0500

    202105071030

--- Practice/practice.js
+++ Practice/practice.js
@@ -1,5 +1,5 @@
 const clickParagraph = document.getElementById('click');
-console.log(clickParagraph);
-/*clickParagraph.addEventListener('click',() => console.log('click') );
+
+clickParagraph.addEventListener('click',() => console.log('click') );
 clickParagraph.addEventListener('mousedown',() => console.log('down') );
-clickParagraph.addEventListener('mouseup',() => console.log('up') );*/
+clickParagraph.addEventListener('mouseup',() => console.log('up') );
