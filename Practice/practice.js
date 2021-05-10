b9bf24693c53a2b8ac2a585135f401e4ad5f1cf9
jwilson5153 <jwilson5153@yahoo.com>
2021-05-10 10:40:05 -0500

    today

--- Practice/practice.js
+++ Practice/practice.js
@@ -1,10 +1,9 @@
-/*const clickParagraph = document.getElementById('click');
-clickParagraph.addEventListener('click', () => console.log('click'));
-clickParagraph.addEventListener('mousedown', () => console.log('down'));
-clickParagraph.addEventListener('mouseup', () => console.log('up'));
-
-function changeColor(newColor) {
-  var elem = document.getElementById('para');
-  console.log(elem);
-  elem.style.color = newColor;
-}*/
+const clickParagraph = document.getElementById('click');
+ clickParagraph.addEventListener('click', () => console.log('click'));
+ clickParagraph.addEventListener('mousedown', () => console.log('down'));
+ clickParagraph.addEventListener('mouseup', () => console.log('up'));
+ function changeColor(newColor) {
+   var elem = document.getElementById('para');
+   console.log(elem);
+   elem.style.color = newColor;
+ }
