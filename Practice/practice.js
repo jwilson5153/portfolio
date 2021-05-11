d135b8b4f71d0b188032bf3595df945ab886786c
jwilson5153 <jwilson5153@yahoo.com>
2021-05-10 21:48:31 -0500

    20210521:48

--- Practice/practice.js
+++ Practice/practice.js
@@ -1,2 +1,20 @@
+const form = document.forms['search'];
 const input = form.elements.searchInput;
-input.addEventListener('focus', () => alert('focused'), false);
+
+form.addEventListener ('submit', search, false);
+function search() {
+    alert(' Form Submitted');
+}
+function search(event) {
+  alert(`You Searched for: ${input.value}`);
+    event.preventDefault();
+}
+input.addEventListener('focus', function(){
+  if (input.value==='Search Here') {
+      input.value = '' 
+  }
+}, false);
+input.addEventListener('blur', function(){
+  if(input.value === '') {
+      input.value = 'Search Here';
+  } }, false);
